594c7ef224fb12c1b6503ce7ef7f306eb870622a
Sadiq <sadiq.said@student.moringaschool.com>
2023-11-07 11:53:15 +0300

    Completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,26 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        Song.genres.add(self.genre)
+        Song.artists.add(self.artist)
+
+        if self.genre in Song.genre_count:
+            Song.genre_count[self.genre] += 1
+        else:
+            Song.genre_count[self.genre] = 1
+
+        if self.artist in Song.artist_count:
+            Song.artist_count[self.artist] += 1
+        else:
+            Song.artist_count[self.artist] = 1
